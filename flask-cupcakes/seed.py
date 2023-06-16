d7ac78b3682db80b462e0150623aaa2dcd0803bd
Matt Goldstein <123273345+Jetsfan1970@users.noreply.github.com>
2023-06-16 16:40:21 -0700

    Add files via upload

---
+++ flask-cupcakes/seed.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from models import db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+db.init_app(app)
+
+# Create an application context
+with app.app_context():
+    # Perform database operations within the application context
+    db.drop_all()
+    db.create_all()
+
+    c1 = Cupcake(
+        flavor="cherry",
+        size="large",
+        rating=5,
+    )
+
+    c2 = Cupcake(
+        flavor="chocolate",
+        size="small",
+        rating=9,
+        image="https://www.bakedbyrachel.com/wp-content/uploads/2018/01/chocolatecupcakesccfrosting1_bakedbyrachel.jpg"
+    )
+
+    db.session.add_all([c1, c2])
+    db.session.commit()
